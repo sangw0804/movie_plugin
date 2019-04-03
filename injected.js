07acbc62441e8c0c6cbbe51b5dbe22b84243b847
SangWoo <sangw0804@likelion.org>
2019-04-03 22:22:08 +0900

    title clue로 찾기 추가

--- injected.js
+++ injected.js
@@ -1,19 +1,39 @@
+const isThereTwo = (str, char, char2) => {
+  const startIndex = str.indexOf(char);
+  const endIndex = str.indexOf(char2, startIndex + 1);
+
+  return startIndex > -1 && endIndex > -1 && [startIndex + 1, endIndex];
+};
+
+const inspect = raw => {};
+
 const getRankOfCandidates = raw => {
   // 어떻게 html title에서 영화 제목을 뽑아낼 것인가?
   // '' "" <> [] 로 둘러쌓인 문자열 -> 영화 제목일 가능성 가장 높음!
-  
+  const clues = [["'", "'"], ['"', '"'], ['<', '>'], ['[', ']']];
+  const candidates = [];
 
-  return raw;
-}
+  clues.forEach(clue => {
+    const result = isThereTwo(raw, clue[0], clue[1]);
 
-const {innerText: title} = document.getElementsByTagName('title')[0];
+    if (result) {
+      candidates.push(raw.substring(result[0], result[1]));
+    }
+  });
+
+  return candidates;
+};
+
+const { innerText: title } = document.getElementsByTagName('title')[0];
 console.log(title);
 
 // 이 파일에서 dom 작업 모두 처리
 
 const movie = getRankOfCandidates(title);
+if (!movie.length) {
+}
 
 chrome.runtime.sendMessage({
-  action: "getSource",
-  source: movie
+  action: 'getSource',
+  source: movie[0]
 });
--- popup.js
+++ popup.js
@@ -6,21 +6,28 @@
 const msg = document.getElementById('message');
 
 chrome.runtime.onMessage.addListener((request, sender) => {
-  if (request.action == "getSource") {
-    // msg.innerText = JSON.stringify(request.sou;
-    const doc = request.source;
-    msg.innerText = request.source;
+  if (request.action == 'getSource') {
+    const { source } = request;
+    msg.innerText = source;
+    chrome.tabs.create({
+      url: `https://movie.naver.com/movie/search/result.nhn?query=${source}&section=all&ie=utf8`,
+      selected: true
+    });
   }
 });
 
 const onWindowLoad = () => {
-  chrome.tabs.executeScript(null, {
-    file: 'injected.js'
-  }, () => {
-    if (chrome.runtime.lastError) {
-      msg.innerText = chrome.runtime.lastError.message;
+  chrome.tabs.executeScript(
+    null,
+    {
+      file: 'injected.js'
+    },
+    () => {
+      if (chrome.runtime.lastError) {
+        msg.innerText = chrome.runtime.lastError.message;
+      }
     }
-  })
-}
+  );
+};
 
 window.onload = onWindowLoad;
